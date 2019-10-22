ba0181d90405ce3d05a4a4f419bcf34ca4edcab5
Emil <37220310+hellcoder09@users.noreply.github.com>
2019-10-22 14:22:23 +0500

    Add files via upload

---
+++ Graphgenerator.py
@@ -0,0 +1,29 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import math
+def f(x):
+    return -25+82*x-90*pow(x,2)+44*pow(x,3)-8*pow(x,4)+0.7*pow(x,5)
+dpi = 80
+fig = plt.figure(dpi = dpi, figsize = (512 / dpi, 384 / dpi) )
+xmin = -100
+xmax = 100
+ymin = -100
+ymax = 100
+mpl.rcParams.update({'font.size': 10})
+plt.axis([xmin, xmax, ymin, ymax])
+plt.title('graf')
+plt.xlabel('x')
+plt.ylabel('F(x)')
+xs = []
+ur_vals = []
+x = xmin
+while x < xmax:
+    ur_vals += [f(x)]
+    xs += [x]
+    x += 0.1
+plt.plot(xs, ur_vals, color = 'red', linestyle = 'dotted',
+         label = 'x')
+plt.grid(True)
+plt.show()
+plt.legend(loc = 'upper right')
+fig.savefig('graf.png')
---
+++ MPD.py
@@ -0,0 +1,20 @@
+import math
+def f(x):
+    return -25+82*x-90*pow(x,2)+44*pow(x,3)-8*pow(x,4)+0.7*pow(x,5)
+def MDP(f, a, b, e, p):
+    while abs(b - a) >= e:
+        if p == 1:
+            x = (a + b) / 2.0
+            fx = f(x)
+            fa = f(a)
+            if (fx < 0 and fa < 0) or (fx > 0 and fa > 0):
+                a = x
+            else:
+                b = x
+        else:
+            x = a*f(b)
+
+    return x
+x = MDP(f, -1, 1, 1e-8, 1)
+print("Корень:",x)
+print("Значение функции:",f(x))
